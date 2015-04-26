15fbf593977ef398c75a0803370045d3a1d5e851
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-04-26 00:08:51 -0500

    Add initial dashboard controller and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  get 'dashboard'     => 'dashboard#index'
 end
