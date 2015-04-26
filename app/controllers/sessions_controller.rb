f7ce535bf07bfc39b892268d3856e8198ac7e5a3
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-04-25 19:22:07 -0500

    Add initial support for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) 
+      log_in user
+      redirect_to dashboard
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+  end
 end
