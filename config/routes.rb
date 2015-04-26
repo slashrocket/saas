b5688131a858289a4f47780457c8657c7ce48fe7
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-04-26 13:23:09 -0500

    Add constraints to root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
 
-  root 'login#index'
+  root 'dashboard#index', constraints: LoggedInConstraint.new(true),
+                          as: authenticated_root
+  root 'login#index', constraints: LoggedInConstraint.new(false)
 
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-  get 'dashboard'     => 'dashboard#index'
 end
