032f27b9acbff506d603690ac33b7e72b7267e25
Slacker87 <ccnp_pro@yahoo.com>
2015-04-27 20:55:20 +0000

    click on user to get json

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,15 @@
 class DashboardController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, except: [:metrics]
   def index
     if params[:search]
       @slack_users = SlackUser.search(params[:search]).order('created_at DESC')
     else
       @slack_users = SlackUser.all
     end
+  end
+  
+  def details
+      @user = SlackUser.find(params[:id])
+      return render partial: 'details.js.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/login', to: 'login#index', as: 'login'
   match '/edit', to: 'login#edit', via: 'get', as: 'edit_profile'
   match '/update', to: 'login#update', via: 'patch', as: 'update_profile'
+  match '/details/:id', to: 'dashboard#details', via: 'get', as: 'details'
 
   post '/login'   => 'sessions#create'
   delete '/login' => 'sessions#destroy'
