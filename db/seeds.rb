47a512078031891280f2017365bf3c04a6a67fa3
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-04-27 10:15:40 -0500

    Seed an initial user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'foo@bar.com',
+             password: 'foobar',
+             password_confirmation: 'foobar')
