1cc3e2f250d7f1ed839a72a82ae85b8348e40f7f
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-05-03 21:07:02 -0500

    Minor update to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success',
+      error: 'alert-error',
+      alert: 'alert-warning',
+      notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
   end
 
-  def flash_messages(opts = {})
+  def flash_messages(_opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
         concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
